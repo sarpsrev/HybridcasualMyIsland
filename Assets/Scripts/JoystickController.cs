63fbc3171e9cabb29947c3d2b04acb5c3cbda4ae
sarp unalan <91683818+sarpsrev@users.noreply.github.com>
2023-08-07 00:56:45 +0300

    joystick control script added

--- Assets/Scripts/JoystickController.cs
+++ Assets/Scripts/JoystickController.cs
@@ -7,6 +7,9 @@ public class JoystickController : MonoBehaviour
     public RectTransform joystickOutline;
     public RectTransform joystickButton;
     public RectTransform joystickArea;
+    public bool directionChosen;
+    Vector3 newPos;
+    Vector3 movement;
     
     // Start is called before the first frame update
     void Start()
@@ -18,7 +21,6 @@ public class JoystickController : MonoBehaviour
     // Update is called once per frame
      void Update()
     {
-
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -27,16 +29,21 @@ public class JoystickController : MonoBehaviour
             {
 
                 case TouchPhase.Began:
-                    Vector2 localPoint;
-                //
-                    RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickArea, touch.position, null, out localPoint);
+                
+                Vector2 localPoint;
+                
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickArea, touch.position, null, out localPoint);
 
-                    joystickOutline.anchoredPosition = localPoint;
+                joystickOutline.anchoredPosition = localPoint;
 
-                    joystickOutline.gameObject.SetActive(true);
+                joystickOutline.gameObject.SetActive(true);
+
+                
                     break;
 
                 case TouchPhase.Moved:
+                directionChosen = true;
+                movement = touch.deltaPosition;
                 Vector2 movementPos = touch.deltaPosition;
 
                 // Calculating area of joystick outline.               
@@ -44,7 +51,7 @@ public class JoystickController : MonoBehaviour
                 float joystickAreaHeight = joystickOutline.rect.height;
 
                 // Calculating new position of joystick
-                Vector3 newPos = joystickButton.gameObject.transform.localPosition + new Vector3(movementPos.x, movementPos.y, 0f);
+                newPos = joystickButton.gameObject.transform.localPosition + new Vector3(movementPos.x, movementPos.y, 0f);
 
 
                 //Make it move within a certain area with clamp for joystick              
@@ -58,12 +65,38 @@ public class JoystickController : MonoBehaviour
 
                     break;
 
+                case TouchPhase.Stationary:
+                    if(directionChosen)
+                    {
+                    //When user still tapped screen but no moved.    
+                    float movementX = joystickButton.gameObject.transform.localPosition.x/10;
+
+                    float movementY = joystickButton.gameObject.transform.localPosition.y/10;
+
+                    movement = new Vector3(movementX,movementY,0f);
+                    }
+
+                         
+
+                    break; 
+
                 case TouchPhase.Ended:
                     hideJoystick();
+                    movement = Vector3.zero;
+                    directionChosen = false;
                     break;
+   
             }
+
         }
+            
     }
+    public Vector3 getMovePosition()
+    {
+        return movement;
+    }
+
+
 
 
     private void hideJoystick()
